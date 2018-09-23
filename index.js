83cee6c733c688e16e1b5a43cde7ccf875ae26d9
Levi C Uzodike <leviuzodike@gmail.com>
2018-09-23 23:58:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ destructivelyRemoveLastKitten(){
   kitten.pop()
 }
 
+destructivelyRemoveFirstKitten(){
+  kitten.unshift()
+}
+
+destructivelyRemoveFirstKitten(){
+  kitten.unshift()
+}
+
+destructivelyAppendKitten(name){
+  kitten.push(name)
+}
+
+destructivelyPrependKitten(name){
+  kitten.shift(name)
+}
+
 destructivelyRemoveLastKitten(){
   kitten.pop()
+}
+
+destructivelyRemoveFirstKitten(){
+  kitten.unshift()
+}
+
+destructivelyRemoveFirstKitten(){
+  kitten.unshift()
 }
